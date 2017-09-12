ea68b6cf9133dfbbb003f68b2d9883e9d2bdd156
maddiegold <maddiegold@maddies-mbp.fios-router.home>
2017-09-12 10:57:11 -0400

    if statements

--- Question1/src/Class1.java
+++ Question1/src/Class1.java
@@ -8,24 +8,38 @@ public class Class1 {
 		double TD = input.nextDouble();
 		
 		System.out.print("Enter how many total yards: ");
-		double YDS = input.nextDouble();
+		double Yards = input.nextDouble();
 		
 		System.out.print("Enter how many interceptions: ");
 		double INT = input.nextDouble();
 		
 		System.out.print("Enter how many completions: ");
-		double COMP = input.nextDouble();
+		double comp = input.nextDouble();
 		
 		System.out.print("Enter number of passes attempted: ");
 		double ATT = input.nextDouble();
 		
-		double a = (((COMP/ATT)-0.3)*5);
+		double a = (((comp/ATT)-0.3)*5);
+		if (a>2.375)
+			a=2.375;
+		if (a<0)
+			a=0;
 		
-		double b = (((YDS/ATT)-3)*0.25);
+		double b = (((Yards/ATT)-3)*0.25);
+		if (b>2.375)
+			b=2.375;
+		if (b<0)
+			b=0;
 		
 		double c = ((TD/ATT)*20);
+		if (c>2.375)
+			c=2.375;
 		
 		double d = (2.375-((INT/ATT)*25));
+		if (d>2.375)
+			d=2.375;
+		if (d<0)
+			d=0;
 		
 		double PasserRating = (((a+b+c+d)/6)*100);
 		
